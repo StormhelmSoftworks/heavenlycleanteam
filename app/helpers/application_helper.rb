9e846f69620b9086c3d90d5dbf967c8da4890e43
viktharien <viktharien@zoho.com>
2018-09-09 14:18:50 -0500

    add info_card helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def info_card(title, image, desc)
+
+
+
+
+    "<div class='card'>
+      <div class='card-image'>
+      <img src='#{image}'>
+
+      </div>
+      <div class='card-content'>
+        <span class='card-title'>#{title}</span>
+        <a href='#{services_list_path}'>>></a>
+        <p>#{desc}</p>
+                                                                                                                                              </div>
+      </div>
+     </div>".html_safe
+
+
+  end
 end
